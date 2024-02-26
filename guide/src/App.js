89c5f2a602ba136224015ed41b108cddaa36f083
agschmid <agschmid@andrew.cmu.edu>
2024-02-26 09:06:07 -0500

    add sounds and icons

--- guide/src/App.js
+++ guide/src/App.js
@@ -3,17 +3,19 @@ import SoundButton from './SoundButton';
 
 function App() {
   return (
-    <div className='container'>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Look Left'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk Forward'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Look Right'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Stop'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk'></SoundButton>
-      <SoundButton sound = 'sound.mp3' icon='walking.svg' text='Walk'></SoundButton>
-    </div>
+    <>
+      <div className='container'>
+        <SoundButton sound = 'success.mp3' icon='good.svg' text='Success'></SoundButton>
+      </div>
+      <div className='container'>
+        <SoundButton sound = 'L.mp3' icon='left.svg' text='Left'></SoundButton>
+        <SoundButton sound = 'start.mp3' icon='walking.svg' text='Start'></SoundButton>
+        <SoundButton sound = 'R.mp3' icon='right.svg' text='Right'></SoundButton>
+      </div>
+      <div className='container'>
+        <SoundButton sound = 'bad.mp3' icon='bad.svg' text='Fail'></SoundButton>
+      </div>
+    </>
   );
 }
 
